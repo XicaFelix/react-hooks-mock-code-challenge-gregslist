d3e5dd158c1c3e9eb95404aa43f4f6dff7c77224
Xica Felix <xicasfelix@gmail.com>
2022-09-22 21:21:51 -0400

    commit tree 394f64df40748b9ed65ac5450a805021ef1588aa

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [listings, setListings] = useState([]);
+  const [filteredListings, setFilteredListings] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:6001/listings').then(resp=>resp.json()).then((result)=>{
+      setListings(result);
+      setFilteredListings(result);
+    })
+   },[])
+
+  function handleFilterListing(searchTerm){
+    if (searchTerm === '') {
+      setFilteredListings(listings);
+    } else {
+      setFilteredListings(listings.filter((item)=> item.description.toLowerCase().includes(searchTerm.toLowerCase())))
+    }
+  }
+  
+  function handleDeleteListing(listing){
+    setListings(listings.filter(l => {
+      return l.id !== listing.id
+    }))
+
+    setFilteredListings(filteredListings.filter(l => {
+      return l.id !== listing.id
+    }))
+
+    fetch(`http://localhost:6001/listings/${listing.id}`, {
+      method: "DELETE"
+    })
+  }
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onFilterListing = {handleFilterListing}/>
+      <ListingsContainer listings={filteredListings} handleDeleteListing={handleDeleteListing}/>
     </div>
   );
 }
